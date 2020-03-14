c47859b7374a6d85573b162ebfd98b1f0fbac771
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-14 16:42:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -102,6 +102,7 @@ def movies_with_directors_set(source)
   # RETURN:
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  binding.pry
   pp source
 end
 
